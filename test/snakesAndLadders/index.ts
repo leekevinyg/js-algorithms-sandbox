a7d0ee5cfa06a282517c3f26e2fd5356920ebf21
klee2010 <klee2010@gmail.com>
2019-11-14 18:08:26 -0800

    fix typo in test description

--- test/snakesAndLadders/index.ts
+++ test/snakesAndLadders/index.ts
@@ -17,7 +17,7 @@ describe('Snakes and ladders problem', () => {
       expect(quickestWayUp(snakesAndLadders, goalPos)).to.equal(1);
     });
 
-    it('returns 10 dice roll to get to a goal of 60 when no snakes or ladders are on the board ', () => {
+    it('returns 10 dice rolls to get to a goal of 60 when no snakes or ladders are on the board ', () => {
       const goalPos = 60;
       const snakesAndLadders = {};
       expect(quickestWayUp(snakesAndLadders, goalPos)).to.equal(10);
