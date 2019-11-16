c262b2098a956bd0f656d5d03c5342c65f67f3a3
klee2010 <klee2010@gmail.com>
2019-11-16 11:40:14 -0800

    clean up

--- src/courseSchedule/course-schedule.ts
+++ src/courseSchedule/course-schedule.ts
@@ -1,22 +1,20 @@
 import { Graph } from './graph';
 
 function canFinishAllCourses(coursesAndPrereqs : number[][]) : boolean {
-    let canFinishAllCourses : boolean = true;
-
     if (coursesAndPrereqs === null || coursesAndPrereqs.length === 0) {
         return true;
     }
 
     let graph : Graph = new Graph();
-    // first add edges to our graph
     for (let i = 0; i < coursesAndPrereqs.length; i++) {
         graph.addEdge(coursesAndPrereqs[i]);
     }
 
     let startNode : number = coursesAndPrereqs[0][0];
-    // traverse graph with DFS, and return true
-    canFinishAllCourses = !graph.isCyclicDFS(startNode);
-    return canFinishAllCourses;
+    if (graph.isCyclicDFS(startNode)) {
+        return false;
+    }
+    return true;
 }
 
 export default canFinishAllCourses;
