6fe4bd91e51a230a450a406d6b6c9192a987faef
klee2010 <klee2010@gmail.com>
2019-11-19 22:28:20 -0800

    pull out graph test cases

--- test/courseSchedule/index.ts
+++ test/courseSchedule/index.ts
@@ -1,4 +1,5 @@
 import canFinishAllCourses from '../../src/courseSchedule/course-schedule';
+import { Graph } from '../../src/courseSchedule/graph';
 import { expect } from 'chai';
 
 describe('Course Schedule problem', () => {
@@ -13,18 +14,36 @@ describe('Course Schedule problem', () => {
     it('returns false when passed 2 courses that are pre-reqs of each other', () => {
         expect(canFinishAllCourses([[1, 0], [0, 1]])).to.equal(false);
     });
+});
 
+describe('Graph.isCyclicDFS()', () => {
     it('returns true when passed a directed graph with no cycles but a node that is reachable by 2 paths', () => {
         // graph from https://stackoverflow.com/a/2869661/1718640
-        expect(canFinishAllCourses([[0, 3], [3, 4], [4, 2], [0, 1], [1, 2]])).to.equal(true);
+        const g = new Graph();
+        const edges = [[0, 3], [3, 4], [4, 2], [0, 1], [1, 2]];
+        for (let i=0; i<edges.length;i++) {
+            g.addEdge(edges[i]);
+        }
+        const startNode = edges[0][0];
+
+        expect(g.isCyclicDFS(startNode)).to.equal(false);
     });
 
-    it('returns false when passed a cyclic graph', () => {
-        expect(canFinishAllCourses([[0,1], [1,2], [2,0]])).to.equal(false);
-        expect(canFinishAllCourses([[0,1], [0, 2], [1,0], [2,0], [2,3], [3,3]])).to.equal(false);
-    });
+    it('returns false when passed cyclic graphs', () => {
+        const graphA = new Graph();
+        const aEdges = [[0,1], [1,2], [2,0]];
+        for (let i=0; i<aEdges.length;i++) {
+            graphA.addEdge(aEdges[i]);
+        }
+        const startA = aEdges[0][0];
+        expect(graphA.isCyclicDFS(startA)).to.equal(true);
 
-    it('can handle disconnected graphs?', () => {
-
+        const graphB = new Graph();
+        const bEdges = [[0,1], [0, 2], [1,0], [2,0], [2,3], [3,3]];
+        for (let i=0; i<bEdges.length;i++) {
+            graphB.addEdge(bEdges[i]);
+        }
+        const startB = bEdges[0][0];
+        expect(graphB.isCyclicDFS(startB)).to.equal(true);
     });
-});
+})
