0c0491e78a60ad8f5e9a31e08dcccda30b7e3728
klee2010 <klee2010@gmail.com>
2020-02-08 22:10:46 -0800

    use while loop

--- src/containerWithMostWater/index.ts
+++ src/containerWithMostWater/index.ts
@@ -3,21 +3,17 @@ export const containerWithMostWater = (height : number[]) : number => {
     let endIndex = height.length - 1;
     let maxArea = 0;
     
-    for (let i=0; i<height.length;i++) {
-        if (endIndex === startIndex) {
-            break;
-        }
+    while (endIndex > startIndex) {
         let minLength = Math.min(height[startIndex], height[endIndex]);
         let width = endIndex - startIndex;
         let currentArea = minLength * width;
         if (currentArea > maxArea) {
             maxArea = currentArea;
         }
-
         if (height[startIndex] < height[endIndex]) {
-            startIndex = startIndex + 1;
+            startIndex++;
         } else {
-            endIndex = endIndex - 1;
+            endIndex--;
         }
     }
     return maxArea;
