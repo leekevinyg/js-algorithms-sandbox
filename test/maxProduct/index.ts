02c9014711a65f280585dc69b0f6abef7f7eed2a
klee2010 <klee2010@gmail.com>
2020-02-16 11:49:53 -0800

    update maxProduct subarray test cases

--- test/maxProduct/index.ts
+++ test/maxProduct/index.ts
@@ -17,11 +17,14 @@ describe('max product in subarray', () => {
     
     it('returns max product - 2 negative numbers that form max product', () => {
         expect(maxProduct([1, 1, 2, -1, 1, -10])).to.equal(20);
-        expect(maxProduct([-1, 1, -10])).to.equal(10);
     });
     
     it('returns max product - 2 numbers that form max product separated by negative numbers', () => {
         expect(maxProduct([1, 1, 2, -1, -1, 10])).to.equal(20);
         expect(maxProduct([1, 1, 2, -1, 1, 10])).to.equal(10);
+    });
+
+    it('returns max product - array contains 0', () => {
+        expect(maxProduct([3, 0, -2, -20, -20, 3])).to.equal(1200);
     });
 });
