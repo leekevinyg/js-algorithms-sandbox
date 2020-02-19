eca3f6bfc697a47f58730d28d4b4d0de9589541c
klee2010 <klee2010@gmail.com>
2020-02-19 00:15:13 -0800

    fix binary search bug

--- src/twoSumSortedArray/index.ts
+++ src/twoSumSortedArray/index.ts
@@ -20,7 +20,7 @@ export const twoSumBinarySearch = (numbers : number[], target : number) : number
         let complement = target - num;
         let indexOfComplement = search(numbers, complement);
         if (indexOfComplement >= 0) {
-            return [i, indexOfComplement];
+            return [i + 1, indexOfComplement + 1];
         }
     }
     return [];
@@ -38,15 +38,13 @@ const search = (numbers : number[], target : number) : number => {
     let low = 0;
     let high = numbers.length - 1;
     while (low <= high) {
-        let mid = Math.ceil(high / 2);
-        if (numbers[mid] > target) {
-            // target is to the right of mid
+        let mid = Math.ceil(low + ((high - low) / 2));
+        if (numbers[mid] === target) {
+            return mid;
+        } else if (numbers[mid] > target) {
             high = mid - 1;
         } else if (numbers[mid] < target) {
-            // target is to the left of mid
             low = mid + 1;
-        } else {
-            return mid;
         }
     }
     return -1;
