791bd9f37f8b4e808dceb400c7a7276e19035e91
klee2010 <klee2010@gmail.com>
2020-02-19 00:14:06 -0800

    handle min rotated sorted array edge case

--- src/minRotatedSortedArray/index.ts
+++ src/minRotatedSortedArray/index.ts
@@ -2,6 +2,10 @@ export const minRotatedSortedArray = (nums : number[]) : number => {
     if (!nums || nums.length === 0) {
         return 0;
     }
+    
+    if (nums.length === 1) {
+        return nums[0];
+    }
 
     let left = 0;
     let right = nums.length - 1;
