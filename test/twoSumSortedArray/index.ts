d5d5f326cfce058ee2cd83568ac439bd0ffd9308
klee2010 <klee2010@gmail.com>
2020-02-19 00:15:25 -0800

    update test cases

--- test/twoSumSortedArray/index.ts
+++ test/twoSumSortedArray/index.ts
@@ -2,7 +2,8 @@ import { twoSumBinarySearch } from '../../src/twoSumSortedArray'
 import { expect } from 'chai';
 
 describe('two sum binary search', () => {
-    it.only('returns the indices of the numbers that match the target sum', () => {
-        expect(twoSumBinarySearch([2, 7, 11, 15], 9).toString()).to.equal([0, 1].toString());
-    });
+   it('returns the indices of the numbers that match the target sum', () => {
+        expect(twoSumBinarySearch([2, 7, 11, 15], 9).toString()).to.equal([1, 2].toString());
+        expect(twoSumBinarySearch([2, 3, 4], 6).toString()).to.equal([1, 3].toString());
+   });
 });
