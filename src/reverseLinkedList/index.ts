83a3b65cce56450700eac521d873b50595f96aac
klee2010 <klee2010@gmail.com>
2020-04-22 22:43:46 -0700

    add node interface

--- src/reverseLinkedList/index.ts
+++ src/reverseLinkedList/index.ts
@@ -1,8 +1,13 @@
-export const reverseLinkedList = (head) => {
+interface Node {
+    val: number,
+    next?: Node,
+}
+
+export const reverseLinkedList = (head : Node) : Node => {
     // traverse the list and turn the pointers the other way.
-    let prev = null;
-    let curr = head;
-    let next = null;
+    let prev : Node = null;
+    let curr : Node = head;
+    let next : Node = null;
     while (curr !== null) {
         // save the next item we need to move to
         next = curr.next;
