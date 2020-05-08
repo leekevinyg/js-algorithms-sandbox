c018c98ca8793425df2f8db9ea145d3e0d8052bb
klee2010 <klee2010@gmail.com>
2020-05-08 15:14:24 -0700

    sliding window solution to smallest subarray with given sum

--- src/smallestSubArrayWithGivenSum/index.ts
+++ src/smallestSubArrayWithGivenSum/index.ts
@@ -21,3 +21,27 @@ export const smallest = function(s : number, arr : number[]) {
     }
     return minLength;
   };
+
+export const smallest_sliding_window = function(s : number, arr : number[]) {
+    if (arr.length === 0) return 0;
+    let minLength = Number.POSITIVE_INFINITY;
+    let left = 0;
+    let right = 0;
+    let sum = 0;
+    while (right < arr.length && left < arr.length) {
+        if (left === right) {
+            sum = arr[right];
+        } else {
+            sum+=arr[right]
+        }
+        if (sum < s) {
+            right++;
+        } else if (sum >= s) {
+            const length = right - left + 1;
+            minLength = Math.min(length, minLength);
+            sum-=arr[left];
+            left++;
+        }
+    }
+    return minLength;
+  };
--- test/smallestSubArrayWithGivenSum/index.ts
+++ test/smallestSubArrayWithGivenSum/index.ts
@@ -1,8 +1,8 @@
-import { smallest } from '../../src/smallestSubArrayWithGivenSum';
+import { smallest, smallest_sliding_window } from '../../src/smallestSubArrayWithGivenSum';
 import { expect } from 'chai';
 
 describe('smallest subarray with given sum', () => {
     it.only('returns smallest subarray with given sum', () => {
-        expect(smallest(7, [2,1,5,2,3,2])).to.equal(2);
+        expect(smallest_sliding_window(7, [2,1,5,2,3,2])).to.equal(2);
     });
 });
