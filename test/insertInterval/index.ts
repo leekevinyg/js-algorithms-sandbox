3bb7e7dac4c023504b9f7fba914c551445077acf
klee2010 <klee2010@gmail.com>
2020-05-16 20:48:23 -0700

    add test cases for insert interval

---
+++ test/insertInterval/index.ts
@@ -0,0 +1,11 @@
+import { insert } from '../../src/insertInterval';
+import { expect } from 'chai';
+
+describe('insert interval', () => {
+    it.only('inserts (and merges as necessary) a new interval into a sorted list of intervals', () => {
+        let intervals = [[1,2], [5,6], [7,8]];
+        let newInterval = [3,8];
+        let expectedInterval = [[1,2], [3,8]].toString();
+        expect(insert(intervals, newInterval).toString()).equal(expectedInterval);
+    });
+})
