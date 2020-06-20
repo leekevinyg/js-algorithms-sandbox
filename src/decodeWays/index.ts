be23d9f6c06155ca2bb5692b332ed3305efdac97
klee2010 <klee2010@gmail.com>
2020-06-20 14:43:12 -0700

    add memoized DP solution for decode ways

--- src/decodeWays/index.ts
+++ src/decodeWays/index.ts
@@ -1,29 +1,63 @@
-/**
- * @param {string} s
- * @return {number}
- */
-export const numDecodings = function(s) {
-    let numDecodings = 0;
-    let totalLength = s.length;
+var numDecodings = function(s : string) {
+    let totalLength : number = s.length;
+    let memo : Map<string, number> = new Map();
+
     const isValid = (string) => {
         return parseInt(string) > 0 && parseInt(string) <= 26;
     };
-    const generateDecodings = (s, index) => {
-        if (index === totalLength) {
-            numDecodings++;
-            return;
-        }
-        for (let i=1; i<=s.length; i++) {
-            let decodedWord = s.slice(0, i);
-            let newWord = s.substr(i);
-            if (isValid(decodedWord)) {
-                generateDecodings(newWord, index + i);
-            } else {
-                return;
-            }
+    
+    const memoize = (key, value) => {
+        if (memo.get(key)) {
+            memo.set(key, memo.get(key) + value)
+        } else {
+            memo.set(key, 1)
         }
     }
 
-    generateDecodings(s, 0);
-    return numDecodings;
+    const generateDecodings = (strNode) => {
+        const { s, parent } = strNode;
+        
+        if (memo.has(s)) {
+            if (memo.get(s)) {
+                let root = strNode.parent;
+                memoize(root.s, memo.get(s));
+                while (root.parent !== null) {
+                    root = root.parent;
+                    memoize(root.s, memo.get(s));
+                }
+            }
+            return;
+        }
+        
+        if (s.length === 0) {
+            let root = strNode.parent;
+            memoize(root.s, 1)
+            while (root.parent !== null) {
+                root = root.parent;
+                memoize(root.s, 1);
+            }
+
+            return;
+        }
+        
+        let index = 1;
+        while (index <= s.length && index <= 2) {
+            const part = s.slice(0, index);
+            if (isValid(part)) {
+                generateDecodings({
+                    s: s.substr(index),
+                    parent: strNode
+                });
+                index++;
+            } else {
+                return 0;
+            }
+        }
+    }
+        
+    generateDecodings({
+        s: s,
+        parent: null,
+    });
+    return memo.get(s) ? memo.get(s) : 0
 };
