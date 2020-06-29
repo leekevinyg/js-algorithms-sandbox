583154bca0eea62faa77250f3d3c089d35429352
klee2010 <klee2010@gmail.com>
2020-06-28 17:57:09 -0700

    3 sum closest 2 pointer solution

---
+++ src/3SumClosest/index.ts
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+export const threeSumClosest = function(nums : number[], target : number) : number {
+    nums.sort((a, b) => a - b);
+    
+    let minDiff : number = Number.POSITIVE_INFINITY;
+    let ans : number = 0;
+    for (let i=0; i<nums.length - 2; i++) { 
+        let left : number = i + 1;
+        let right : number = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+            if (sum === target) {
+                return sum;
+            } else {
+                const diff = Math.abs(target - sum);
+                if (diff < minDiff) {
+                    minDiff = diff;
+                    ans = sum;
+                }
+            }
+
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+    return ans;
+};
